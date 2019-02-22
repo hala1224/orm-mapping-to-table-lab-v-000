f0978b2cf9ca985d7ed38c06b735182772126e8f
Natalie Halawani <hala1224@gmail.com>
2019-02-22 01:38:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,12 +12,14 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students(
+    CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
-      )
-    SQL 
-      DB[:conn].execute(sql)
-    end
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  
 end
